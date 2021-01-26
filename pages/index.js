04def2800c2e763da6b10c673dc41f33493cb802
Elysson MR <elyssonmr@gmail.com>
2021-01-26 00:33:11 -0300

    Add first class files

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
 import styled from 'styled-components'
 
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+
+
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
+
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            Quiz Batman Arkham Series
+          </Widget.Header>
+          <Widget.Content>
+            <h1>Quiz Batman Arkham Series</h1>
+
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Pontuação</h1>
+
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl='https://github.com/elyssonmr/batman-quiz'/>
+    </QuizBackground>
+  )
 }
